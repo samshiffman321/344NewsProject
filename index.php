69472cf354127d6f28758f86bc833015f07fe5e0
Sam Shiffman <samshiffman321@gmail.com>
2016-04-18 19:07:55 -0400

    grid system 1

--- index.php
+++ index.php
@@ -8,15 +8,16 @@
 </head>
 <body>
 
+	<div class="col-3">
+		<form id="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+			<input type="checkbox" class="checkbox" name="check_list[]" value="http://sports.espn.go.com/espn/rss/news"><label>ESPN</label><br/>
+			<input type="checkbox" class="checkbox" name="check_list[]" value="http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"><label>NYT</label><br/>
+			<input type="checkbox" class="checkbox" name="check_list[]" value="http://rss.cnn.com/rss/cnn_topstories.rss"><label>CNN</label><br/>
+			<input type="checkbox" class="checkbox" name="check_list[]" value="http://www.wired.co.uk/news/rss"><label>WIRED</label><br/>
+		</form>
+	</div>
 
-	<form id="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-		<input type="checkbox" class="checkbox" name="check_list[]" value="http://sports.espn.go.com/espn/rss/news"><label>ESPN</label><br/>
-		<input type="checkbox" class="checkbox" name="check_list[]" value="http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"><label>NYT</label><br/>
-		<input type="checkbox" class="checkbox" name="check_list[]" value="http://rss.cnn.com/rss/cnn_topstories.rss"><label>CNN</label><br/>
-		<input type="checkbox" class="checkbox" name="check_list[]" value="http://www.wired.co.uk/news/rss"><label>WIRED</label><br/>
-	</form>
-
-	<div id="rssOutput">
+	<div id="rssOutput col-9">
 
 
   </div>
